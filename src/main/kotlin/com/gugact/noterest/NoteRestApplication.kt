895b349580715cd13bc01666c3d30712a3a1040f
Gustavo Carvalho Tremiliosi <gustavo.a.carvalho@accenture.com>
2022-07-01 15:45:37 +0200

    base path

--- src/main/kotlin/com/gugact/noterest/NoteRestApplication.kt
+++ src/main/kotlin/com/gugact/noterest/NoteRestApplication.kt
@@ -3,10 +3,18 @@ package com.gugact.noterest
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
 class NoteRestApplication
 
 fun main(args: Array<String>) {
-	runApplication<NoteRestApplication>(*args)
+    runApplication<NoteRestApplication>(*args)
+}
+
+@RestController
+class HelloController() {
+    @GetMapping("/")
+    fun hello() = "Hello world"
 }
